68c29eaf4aaaa7836c1080b5143b614735c8c684
SamrahS <samrahsajid16@gmail.com>
2018-11-06 02:01:42 +0500

    test

--- app/src/main/java/project/smd/studysync/MainActivity.java
+++ app/src/main/java/project/smd/studysync/MainActivity.java
@@ -2,9 +2,10 @@ package project.smd.studysync;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+        Button B;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
