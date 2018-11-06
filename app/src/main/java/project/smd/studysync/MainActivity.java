8426ad0adc696897fdfac32ab83ab6888c5e323c
SamrahS <samrahsajid16@gmail.com>
2018-11-06 13:31:34 +0500

    test ss

--- app/src/main/java/project/smd/studysync/MainActivity.java
+++ app/src/main/java/project/smd/studysync/MainActivity.java
@@ -4,8 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
         Button B;
+        Text t;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
